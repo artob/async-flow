d94e63da4732c55008998936df9d48dfa3fb8e2a
Arto Bendiken <arto@bendiken.net>
2026-01-27 08:21:27 -0600

    Improve the debug output of model systems.

--- src/model/system_definition.rs
+++ src/model/system_definition.rs
@@ -1,13 +1,12 @@
 // This is free and unencumbered software released into the public domain.
 
 use super::{BlockDefinition, InputId, OutputId, SystemBuilder};
-use alloc::{collections::BTreeSet, rc::Rc, vec::Vec};
-use derive_more::Debug;
+use alloc::{borrow::Cow, collections::BTreeSet, rc::Rc, vec::Vec};
+use core::fmt::Debug;
 
 /// A system definition.
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Default)]
 pub struct SystemDefinition {
-    #[debug(skip)]
     pub(crate) blocks: Vec<Rc<dyn BlockDefinition>>,
     pub(crate) connections: BTreeSet<(OutputId, InputId)>,
 }
@@ -16,5 +15,26 @@ impl SystemDefinition {
     /// Returns a system builder.
     pub fn build() -> SystemBuilder {
         SystemBuilder::new()
+    }
+}
+
+impl Debug for SystemDefinition {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        struct BlockName<'a>(Cow<'a, str>);
+        impl<'a> Debug for BlockName<'a> {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.debug_tuple(&self.0).finish()
+            }
+        }
+
+        let block_names = self
+            .blocks
+            .iter()
+            .map(|block| BlockName(block.name()))
+            .collect::<Vec<_>>();
+        f.debug_struct("SystemDefinition")
+            .field("blocks", &block_names)
+            .field("connections", &self.connections)
+            .finish()
     }
 }
